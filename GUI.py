0c7de335d499d8eb47cca94125cd1ab2a5dfd0a3
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2022-04-03 13:30:14 +0700

    main nyusul + kurang[i]

--- GUI.py
+++ GUI.py
@@ -2,6 +2,11 @@ import tkinter as Tk
 import numpy as np
 import time
 from tkinter import messagebox
+import sys
+
+#ganti dengan path dimana menyimpan algoritma branch and bound
+sys.path.insert(0, 'D:\Semester4\StrategiAlgoritma\Tucil3\BnB')
+from solver import BranchandBound as bb, node, prioQueue
 
 class GUI(Tk.Frame):
     #me-nginisiasi parent
@@ -22,7 +27,6 @@ class GUI(Tk.Frame):
         for j in range(0,self.a.shape[0]):
             for k in range(0,self.a.shape[1]):
                 root.update()
-                time.sleep(0.05)
                 test_integer = self.matrix[matindex]
                 integer = test_integer[j][k]
                 if(integer != 0): 
@@ -31,9 +35,9 @@ class GUI(Tk.Frame):
                     self.b = Tk.Button(self.frame, text = " ", height= 5, width=10, bg='blue', font = ('4'))
                 self.b.grid(row=j,  column= k)
         self.btn1= Tk.Button(self.frame, text="Next" , command = lambda : self.changestate(matindex, len_matrix), font = ('Helvetica'))
-        self.btn1.grid(row=3,  column=1)
+        self.btn1.grid(row=4,  column=1)
         self.btn2= Tk.Button(self.frame, text="Previous", command = lambda : self.changestate2(matindex, len_matrix), font = ('Helvetica'))
-        self.btn2.grid(row=3,  column=0)
+        self.btn2.grid(row=4,  column=0)
     
     #melakukan change state jika user menekan tombol "previous"
     def changestate2(self, matindex, len_matrix):
@@ -44,7 +48,6 @@ class GUI(Tk.Frame):
         for j in range(0,self.a.shape[0]):
             for k in range(0,self.a.shape[1]):
                 root.update()
-                time.sleep(0.05)
                 test_integer = self.matrix[matindex]
                 integer = test_integer[j][k]
                 if(integer != 0): 
@@ -53,9 +56,9 @@ class GUI(Tk.Frame):
                     self.b = Tk.Button(self.frame, text = " ", height= 5, width=10, bg='blue', font = ('4'))
                 self.b.grid(row=j,  column= k)
         self.btn1= Tk.Button(self.frame, text="Next" , command = lambda : self.changestate(matindex, len_matrix), font = ('Helvetica'))
-        self.btn1.grid(row=3,  column=1)
+        self.btn1.grid(row=4,  column=1)
         self.btn2= Tk.Button(self.frame, text="Previous", command = lambda : self.changestate2(matindex, len_matrix), font = ('Helvetica'))
-        self.btn2.grid(row=3,  column=0)
+        self.btn2.grid(row=4,  column=0)
 
     #untuk memulai pemrosesan pembuatan GUI    
     def initialize(self):
@@ -65,13 +68,13 @@ class GUI(Tk.Frame):
         self.parent.title("15-Puzzle Branch and Bound")       
         self.parent.grid_rowconfigure(1,weight=1)
         self.parent.grid_columnconfigure(1,weight=1)
-        root.geometry("400x400")
+        root.geometry("700x700")
 
         self.frame = Tk.Frame(self.parent)  
         self.frame.pack(fill=Tk.X, padx=1, pady=1)
 
-        # Membuat array 3, 3
-        self.a = np.zeros((3,3))
+        # Membuat array 4, 4
+        self.a = np.zeros((4,4))
         len_matrix = len(self.matrix)
         index_matrix = 1
         for j in range(0,self.a.shape[0]):
@@ -84,6 +87,6 @@ class GUI(Tk.Frame):
                     self.b = Tk.Button(self.frame, text = "  ", height= 5, width=10, bg='blue', font = ('4'))
                 self.b.grid(row=j,  column= k)
         self.btn1= Tk.Button(self.frame, text="Next" , command = lambda : self.changestate(index_matrix, len_matrix), font = ('Helvetica'))
-        self.btn1.grid(row=3,  column=1)
+        self.btn1.grid(row=4,  column=1)
         self.btn2= Tk.Button(self.frame, text="Previous", command = lambda : self.changestate2(index_matrix, len_matrix), font = ('Helvetica'))
-        self.btn2.grid(row=3,  column=0)
+        self.btn2.grid(row=4,  column=0)
